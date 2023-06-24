a21bb03b4177ab3f879cc86c2939be431cc157ae
VitaliySaburdo <vsaburdo@gmail.com>
2023-06-24 13:46:09 +0300

    start

---
+++ Lesson_1/app.js
@@ -0,0 +1,14 @@
+var button = document.querySelector("button");
+var input1 = document.getElementById("num1");
+var input2 = document.getElementById("num2");
+function add(num1, num2) {
+    return num1 + num2;
+}
+if (button) {
+    button.addEventListener("click", function () {
+        console.log(add(+input1.value, +input2.value));
+    });
+}
+else {
+    console.log("Button element not found.");
+}
---
+++ Lesson_2/app.ts
@@ -0,0 +1,34 @@
+/**
+  |============================
+  | Simple type of data structure
+  |============================
+*/
+
+let num: number;
+
+num = 10;
+
+let str: string;
+
+str = "String";
+
+let bool: boolean;
+
+bool = true;
+
+// No reason to indicate type of data
+const person = "Max";
+
+/**
+  |============================
+  | Hard type of data structure
+  |============================
+*/
+
+let arr: string[] = [];
+
+arr = ["string"];
+
+let arrNum: number[] = [];
+
+arrNum = [1, 2];

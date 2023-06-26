1a54b3004bd0acc0ea119021319de3d3dd79daa2
VitaliySaburdo <vsaburdo@gmail.com>
2023-06-26 17:46:47 +0300

    add

--- Lesson_2/app.js
+++ Lesson_2/app.js
@@ -3,148 +3,103 @@
   | Simple type of data structure
   |============================
 */
-
-let num: number;
-
+var num;
 num = 10;
-
-let str: string;
-
+var str;
 str = "String";
-
-let bool: boolean;
-
+var bool;
 bool = true;
-
 // No reason to indicate type of data
-const person = "Max";
-
+var person = "Max";
 /**
   |============================
   | Hard type of data structure
   |============================
 */
-
-let arr: string[] = [];
-
+var arr = [];
 arr = ["string"];
-
-let arrNum: number[] = [];
-
+var arrNum = [];
 arrNum = [1, 2];
-
-let arrAny: any[] = [];
-
+var arrAny = [];
 arrAny = [1, "2", { name: "string" }];
-
-let arrObject: { name: string }[];
-
+var arrObject;
 arrObject = [{ name: "Max" }, { name: "Alex" }];
-
-let obj: { value: string };
-
+var obj;
 obj = { value: "string" };
-
 obj.value;
-
-let db: {
-  id: number;
-  title: string;
-  // ? means that the field is optional
-  info?: {
-    date: Date;
-    isNew?: boolean;
-  };
-};
-
+var db;
 // no errors here
 db = {
-  id: 1,
-  title: "New product",
-  info: {
-    date: new Date(),
-    // isNew: true
-  },
+    id: 1,
+    title: "New product",
+    info: {
+        date: new Date(),
+        // isNew: true
+    },
 };
-
 /**
   |============================
   | Any
   |============================
 */
-
-let some: any;
-
+var some;
 // no errors here
 some = 1;
 some = "string";
 some = true;
 some = [];
 some = {};
-
 /**
   |============================
   | Unknown
   |============================
 */
-
-let something: unknown;
-
+var something;
 something = 10;
-
 something = 'string';
-
-let string: string;
-
+var string;
 // Type guard
-
 if (typeof something === 'string') {
-  string = something
+    string = something;
 }
-
 /**
   |============================
   | Tuple
   |============================
 */
-
-
-let fixed: ['string', number];
-
+var fixed;
 fixed = ['string', 2];
-
 /**
   |============================
   | Enum
   |============================
 */
-
-enum Toggle { ENABLE, DISABLE };
-
-const service = {
-  status: Toggle.ENABLE
+var Toggle;
+(function (Toggle) {
+    Toggle[Toggle["ENABLE"] = 0] = "ENABLE";
+    Toggle[Toggle["DISABLE"] = 1] = "DISABLE";
+})(Toggle || (Toggle = {}));
+;
+var service = {
+    status: Toggle.ENABLE
 };
-
 if (service.status === Toggle.ENABLE) {
-  console.log('it`s active');
-};
-
+    console.log('it`s active');
+}
+;
 /**
   |============================
   | Union type
   |============================
 */
-
-let union: number | string;
-
+var union;
 union = 10;
-union = "Hello World"
-
-function combine(param1: string | number, param2: string | number) {
-  if (typeof param1 === 'string' || typeof param2 === 'string') {
-    return param1.toString() + param2.toString();
-  }
-  return param1 + param2;
-};
-
-console.log(combine(1, 2))
+union = "Hello World";
+function combine(param1, param2) {
+    if (typeof param1 === 'string' || typeof param2 === 'string') {
+        return param1.toString() + param2.toString();
+    }
+    return param1 + param2;
+}
+;
+console.log(combine(1, 2));

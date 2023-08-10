01c2f3454f59cee1bbfb95d535d8ea35687b13bd
VitaliySaburdo <vsaburdo@gmail.com>
2023-08-10 14:32:25 +0300

    fix

---
+++ Practise_2/app.js
@@ -0,0 +1,4 @@
+var age;
+age = 50;
+var name;
+name = "Max";
---
+++ Practise_2/app.ts
@@ -0,0 +1,14 @@
+let age: number;
+age = 50
+let Name: string;
+Name = 'Max'
+let toggle: boolean;
+toggle = true;
+let empty: null;
+empty = null;
+let notInitialize: undefined;
+notInitialize = undefined;
+let callback: (a: number) => number;
+callback = (a) => {
+    return 100 + a;
+};
